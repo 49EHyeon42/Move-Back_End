b9cb109fca0cd8c2c3622dceb0982f72eef0cedd
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-09-09 20:27:50 +0900

    feat: ObjectMapper, PasswordEncoder 컨테이너 등록

---
+++ src/main/java/dev/ehyeon/move/global/GlobalConfiguration.java
@@ -0,0 +1,21 @@
+package dev.ehyeon.move.global;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class GlobalConfiguration {
+
+    @Bean
+    public ObjectMapper getObjectMapper() {
+        return new ObjectMapper();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
