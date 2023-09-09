1451186304292550c6c329ff917d28999af3dc2a
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-09-09 20:49:56 +0900

    feat: TestController 임시 추가 (인증 확인용)

---
+++ src/main/java/dev/ehyeon/move/controller/TestController.java
@@ -0,0 +1,19 @@
+package dev.ehyeon.move.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String root() {
+        return "인증 성공";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, " + SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+}
