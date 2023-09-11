59b7a60ffc27dc8dcd155895e15fe136355a2ae7
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-09-11 13:56:11 +0900

    feat: SignInRequest 검증 추가

--- src/main/java/dev/ehyeon/move/security/local/SignInAuthenticationFilter.java
+++ src/main/java/dev/ehyeon/move/security/local/SignInAuthenticationFilter.java
@@ -1,6 +1,9 @@
 package dev.ehyeon.move.security.local;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
+import dev.ehyeon.move.security.exception.IllegalArgumentAuthenticationException;
+import dev.ehyeon.move.security.exception.SecurityErrorCode;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -23,9 +26,25 @@ public class SignInAuthenticationFilter extends AbstractAuthenticationProcessing
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException {
-        // TODO catch MismatchedInputException, null etc
-        SignInRequest signInRequest = objectMapper.readValue(request.getInputStream(), SignInRequest.class);
+        // TODO 예외 핸들링 수정, AuthenticationEntryPoint에 예외가 전달되지 않음
+        try {
+            SignInRequest signInRequest = objectMapper.readValue(request.getInputStream(), SignInRequest.class);
 
-        return getAuthenticationManager().authenticate(new SignInAuthenticationToken(signInRequest.getEmail(), signInRequest.getPassword()));
+            validateSignInRequest(signInRequest);
+
+            return getAuthenticationManager().authenticate(new SignInAuthenticationToken(signInRequest.getEmail(), signInRequest.getPassword()));
+        } catch (MismatchedInputException | IllegalArgumentException e) {
+            throw new IllegalArgumentAuthenticationException(SecurityErrorCode.ILLEGAL_ARGUMENT.getMessage());
+        }
+    }
+
+    private void validateSignInRequest(SignInRequest request) {
+        if (validateString(request.getEmail()) || validateString(request.getPassword())) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean validateString(String string) {
+        return string == null || string.isBlank();
     }
 }
