12d306043381fc5848270f49eb3b0e30e77a002a
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-09-12 22:21:19 +0900

    feat: 날짜별 조회 기능 추가

--- src/main/java/dev/ehyeon/move/repository/RecordRepository.java
+++ src/main/java/dev/ehyeon/move/repository/RecordRepository.java
@@ -3,9 +3,12 @@ package dev.ehyeon.move.repository;
 import dev.ehyeon.move.entity.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
 import java.util.List;
 
 public interface RecordRepository extends JpaRepository<Record, Long> {
 
     List<Record> findAllRecordByMemberId(long memberId);
+
+    List<Record> findAllRecordByMemberIdAndSavedDateBetween(long memberId, Date from, Date to);
 }
