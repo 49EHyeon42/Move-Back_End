b8f922af20ae9fd38b50d984c5e7761e647bf1c1
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-09-12 21:40:44 +0900

    feat: record repository 추가

---
+++ src/main/java/dev/ehyeon/move/repository/RecordRepository.java
@@ -0,0 +1,11 @@
+package dev.ehyeon.move.repository;
+
+import dev.ehyeon.move.entity.Record;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RecordRepository extends JpaRepository<Record, Long> {
+
+    List<Record> findAllRecordByMemberId(long memberId);
+}
