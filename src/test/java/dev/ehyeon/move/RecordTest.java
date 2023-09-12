26bc475a1f73640fcf563b7e0b2504793eaef2b0
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-09-12 22:21:35 +0900

    test: 날짜별 조회 기능 테스트 추가

--- src/test/java/dev/ehyeon/move/RecordTest.java
+++ src/test/java/dev/ehyeon/move/RecordTest.java
@@ -12,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -26,8 +28,19 @@ public class RecordTest {
     RecordRepository recordRepository;
 
     @BeforeEach
-    void before() {
-        memberRepository.save(new Member("email@domain.com", "password", Role.MEMBER));
+    void before() throws ParseException {
+        Member savedMember = memberRepository.save(new Member("email@domain.com", "password", Role.MEMBER));
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        recordRepository.save(
+                new Record(savedMember, format.parse("2000-01-01 01:00:00"), 1, 1, 1, 1, 1));
+
+        recordRepository.save(
+                new Record(savedMember, format.parse("2000-01-01 02:00:00"), 2, 2, 2, 2, 2));
+
+        recordRepository.save(
+                new Record(savedMember, format.parse("2000-01-01 03:00:00"), 3, 3, 3, 3, 3));
     }
 
     @Test
@@ -44,6 +57,22 @@ public class RecordTest {
 
         for (Record foundRecord : foundRecords) {
             System.out.println("found record: id = " + foundRecord.getId());
+        }
+    }
+
+    @Test
+    void findAllRecordByMemberIdAndDateBetweenTest() throws ParseException {
+        Member foundMember = memberRepository.findMemberByEmail("email@domain.com")
+                .orElseThrow(MemberNotFoundException::new);
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        List<Record> foundRecords = recordRepository.findAllRecordByMemberIdAndSavedDateBetween(
+                foundMember.getId(),
+                format.parse("2000-01-01 01:00:00"), format.parse("2000-01-01 02:00:00"));
+
+        for (Record foundRecord : foundRecords) {
+            System.out.println("found record: date = " + foundRecord.getSavedDate());
         }
     }
 }
