9ff82d5f78638d2def5a6fa501081479c27513e5
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-09-13 22:30:49 +0900

    feat: 시작 날짜부터 종료 날짜까지 기록을 조회하는 기능 추가

--- src/main/java/dev/ehyeon/move/controller/RecordController.java
+++ src/main/java/dev/ehyeon/move/controller/RecordController.java
@@ -1,18 +1,27 @@
 package dev.ehyeon.move.controller;
 
 import dev.ehyeon.move.request.SaveRecordRequest;
+import dev.ehyeon.move.response.GetRecordResponse;
 import dev.ehyeon.move.service.RecordService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class RecordController {
 
     private final RecordService recordService;
+
+    @GetMapping("/api/record")
+    public List<GetRecordResponse> getRecord(@RequestParam("from") @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime from,
+                                             @RequestParam("to") @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime to) {
+        return recordService.findAllRecordByMemberEmailAndSavedDateBetween(getMemberEmail(), from, to);
+    }
 
     @PostMapping("/api/record")
     public void saveRecord(@RequestBody SaveRecordRequest request) {
--- src/main/java/dev/ehyeon/move/service/RecordService.java
+++ src/main/java/dev/ehyeon/move/service/RecordService.java
@@ -5,10 +5,15 @@ import dev.ehyeon.move.entity.Record;
 import dev.ehyeon.move.repository.MemberRepository;
 import dev.ehyeon.move.repository.RecordRepository;
 import dev.ehyeon.move.request.SaveRecordRequest;
+import dev.ehyeon.move.response.GetRecordResponse;
 import dev.ehyeon.move.security.exception.MemberNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -32,5 +37,21 @@ public class RecordService {
                 request.getAverageSpeed(),
                 request.getStep(),
                 request.getCalorieConsumption()));
+    }
+
+    public List<GetRecordResponse> findAllRecordByMemberEmailAndSavedDateBetween(String email, LocalDateTime from, LocalDateTime to) {
+        Member foundMember = memberRepository.findMemberByEmail(email)
+                .orElseThrow(MemberNotFoundException::new);
+
+        List<Record> foundRecords = recordRepository.findAllRecordByMemberIdAndSavedDateBetween(foundMember.getId(), from, to);
+
+        List<GetRecordResponse> recordResponses = new ArrayList<>();
+
+        for (Record foundRecord : foundRecords) {
+            recordResponses.add(new GetRecordResponse(foundRecord.getSavedDate(), foundRecord.getElapsedTime(), foundRecord.getTotalTravelDistance(),
+                    foundRecord.getAverageSpeed(), foundRecord.getStep(), foundRecord.getCalorieConsumption()));
+        }
+
+        return recordResponses;
     }
 }
