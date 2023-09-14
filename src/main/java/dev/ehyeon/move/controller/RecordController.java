4946186f195aaea4a5c706ada47f014f902d259d
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-09-14 17:01:30 +0900

    feat: get record 추가

--- src/main/java/dev/ehyeon/move/controller/RecordController.java
+++ src/main/java/dev/ehyeon/move/controller/RecordController.java
@@ -1,14 +1,16 @@
 package dev.ehyeon.move.controller;
 
+import dev.ehyeon.move.request.GetRecordRequest;
 import dev.ehyeon.move.request.SaveRecordRequest;
 import dev.ehyeon.move.response.GetRecordResponse;
 import dev.ehyeon.move.service.RecordService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -18,9 +20,8 @@ public class RecordController {
     private final RecordService recordService;
 
     @GetMapping("/api/record")
-    public List<GetRecordResponse> getRecord(@RequestParam("from") @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime from,
-                                             @RequestParam("to") @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime to) {
-        return recordService.findAllRecordByMemberEmailAndSavedDateBetween(getMemberEmail(), from, to);
+    public List<GetRecordResponse> getRecord(GetRecordRequest request) {
+        return recordService.findAllRecordByMemberEmailAndSavedDateBetween(getMemberEmail(), request.getFrom(), request.getTo());
     }
 
     @PostMapping("/api/record")
