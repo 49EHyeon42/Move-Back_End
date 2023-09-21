ee55c18c87ce06bf8efaca7180abf1475b47adb7
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-09-21 17:15:20 +0900

    feat: 주소로 moveStop 찾는 메서드 추가

--- src/main/java/dev/ehyeon/move/repository/MoveStopRepository.java
+++ src/main/java/dev/ehyeon/move/repository/MoveStopRepository.java
@@ -3,5 +3,9 @@ package dev.ehyeon.move.repository;
 import dev.ehyeon.move.entity.MoveStop;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MoveStopRepository extends JpaRepository<MoveStop, Long> {
+
+    List<MoveStop> findMoveStopByAddress(String address);
 }
