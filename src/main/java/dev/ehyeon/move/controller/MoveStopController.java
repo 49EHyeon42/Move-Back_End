a376f8dc1cfd61a24c3250a88ed865e61c8ccf28
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-09-21 21:44:46 +0900

    feat: 남서쪽, 북동쪽 좌표 범위 내로 조회하는 request, response, service, controller 추가

--- src/main/java/dev/ehyeon/move/controller/MoveStopController.java
+++ src/main/java/dev/ehyeon/move/controller/MoveStopController.java
@@ -1,18 +1,28 @@
 package dev.ehyeon.move.controller;
 
+import dev.ehyeon.move.request.GetMoveStopRequest;
 import dev.ehyeon.move.request.RegisterMoveStopRequest;
+import dev.ehyeon.move.response.GetMoveStopResponse;
 import dev.ehyeon.move.service.MoveStopService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class MoveStopController {
 
     private final MoveStopService moveStopService;
+
+    @GetMapping("/move_stop")
+    public List<GetMoveStopResponse> getMoveStop(GetMoveStopRequest request) {
+        return moveStopService.getMoveStop(request);
+    }
 
     @PostMapping("/move_stop")
     public void registerMoveStop(@RequestBody @Validated RegisterMoveStopRequest request) {
