55e08abb1683e75bd8efabf12ad157997ba3da3b
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-09-28 12:33:15 +0900

    fix: visitedMoveStop 비활성화

--- src/main/java/dev/ehyeon/move/entity/VisitedMoveStop.java
+++ src/main/java/dev/ehyeon/move/entity/VisitedMoveStop.java
@@ -1,38 +1,39 @@
-package dev.ehyeon.move.entity;
-
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-import java.time.LocalDateTime;
-
-@Entity
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Getter
-public class VisitedMoveStop {
-
-    @Id
-    @GeneratedValue
-    private Long id;
-
-    @ManyToOne
-    @JoinColumn(name = "member_id")
-    private Member member;
-
-    @ManyToOne
-    @JoinColumn(name = "move_stop_id")
-    private MoveStop moveStop;
-
-    private LocalDateTime dateOfLastVisit;
-
-    public VisitedMoveStop(Member member, MoveStop moveStop, LocalDateTime dateOfLastVisit) {
-        this.member = member;
-        this.moveStop = moveStop;
-        this.dateOfLastVisit = dateOfLastVisit;
-    }
-
-    public void updateDateOfLastVisit(LocalDateTime dateOfLastVisit) {
-        this.dateOfLastVisit = dateOfLastVisit;
-    }
-}
+//package dev.ehyeon.move.entity;
+//
+//import dev.ehyeon.move.move_stop.adapter.out.persistence.MoveStopEntity;
+//import lombok.AccessLevel;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//import javax.persistence.*;
+//import java.time.LocalDateTime;
+//
+//@Entity
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//@Getter
+//public class VisitedMoveStop {
+//
+//    @Id
+//    @GeneratedValue
+//    private Long id;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "member_id")
+//    private Member member;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "move_stop_id")
+//    private MoveStopEntity moveStopEntity;
+//
+//    private LocalDateTime dateOfLastVisit;
+//
+//    public VisitedMoveStop(Member member, MoveStopEntity moveStopEntity, LocalDateTime dateOfLastVisit) {
+//        this.member = member;
+//        this.moveStopEntity = moveStopEntity;
+//        this.dateOfLastVisit = dateOfLastVisit;
+//    }
+//
+//    public void updateDateOfLastVisit(LocalDateTime dateOfLastVisit) {
+//        this.dateOfLastVisit = dateOfLastVisit;
+//    }
+//}
--- src/main/java/dev/ehyeon/move/repository/VisitedMoveStopRepository.java
+++ src/main/java/dev/ehyeon/move/repository/VisitedMoveStopRepository.java
@@ -1,11 +1,11 @@
-package dev.ehyeon.move.repository;
-
-import dev.ehyeon.move.entity.VisitedMoveStop;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface VisitedMoveStopRepository extends JpaRepository<VisitedMoveStop, Long> {
-
-    Optional<VisitedMoveStop> findVisitedMoveStopByMemberIdAndMoveStopId(Long memberId, Long moveStopId);
-}
+//package dev.ehyeon.move.repository;
+//
+//import dev.ehyeon.move.entity.VisitedMoveStop;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.Optional;
+//
+//public interface VisitedMoveStopRepository extends JpaRepository<VisitedMoveStop, Long> {
+//
+//    Optional<VisitedMoveStop> findVisitedMoveStopByMemberIdAndMoveStopId(Long memberId, Long moveStopId);
+//}
